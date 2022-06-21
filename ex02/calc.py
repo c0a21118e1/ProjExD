d39aaa868e01d1f29bd0f8142441cec324e486f1
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-06-21 14:50:21 +0900

    問4

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,11 +6,14 @@ def button_click(event):
     tkm.showinfo("", f"{num}のボタンがクリックされました")
 if __name__ == "__main__":
 
-    import tkinter as tk
-    r,c = 0, 0
+    
+    r,c = 1, 0
     root = tk.Tk()
     root.title("超高機能電卓")
     root.geometry("300x500")
+
+    entry = tk.Entry(root, justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3)
     for num in range(9, -1, -1):
         btn = tk.Button(root,text=f"{num}",width=4,height=2,font=("Times New Roman", 30))
         btn.bind("<1>", button_click)
