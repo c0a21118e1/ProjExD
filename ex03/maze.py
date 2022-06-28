667d0835e0ad2e2bddd7834535abb22ef0818306
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-06-28 14:31:03 +0900

    問３

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,5 +7,8 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canvas.pack()
+    tori = tk.PhotoImage(file="ex03/fig/5.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
     canvas.mainloop()
 
