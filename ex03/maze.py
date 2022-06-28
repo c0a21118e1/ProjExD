65ed7c4ece53e217dd3e7f985ac0c59b7ebf55bc
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-06-28 14:54:01 +0900

    問７

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,6 +11,22 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global key, cx, cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -23,5 +39,6 @@ if __name__ == "__main__":
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     canvas.create_image(cx, cy, image=tori, tag="tori")
+    main_proc()
     canvas.mainloop()
 
