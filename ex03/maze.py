7b7ea53230470f00d80e39608968cbba44f307c3
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-06-28 14:40:56 +0900

    問５，６

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,16 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+
+def key_up(event):
+    global key
+    key = ""
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -10,6 +20,8 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="ex03/fig/5.png")
     cx, cy = 300, 400
     key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     canvas.create_image(cx, cy, image=tori, tag="tori")
     canvas.mainloop()
 
