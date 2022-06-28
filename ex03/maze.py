4ef769e3b9c5b206bafa1f6490708dd12a7a54af
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-06-28 14:16:48 +0900

    問１

---
+++ ex03/maze.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("1500x900")
+    root.title("迷えるこうかとん")
+    root.mainloop()
+
