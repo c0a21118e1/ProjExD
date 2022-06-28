b66262e15ef7103bbc41c976f02bf98457cdf36a
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-06-28 14:23:44 +0900

    問２

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,7 +4,8 @@ import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("1500x900")
     root.title("迷えるこうかとん")
-    root.mainloop()
+    canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+    canvas.pack()
+    canvas.mainloop()
 
