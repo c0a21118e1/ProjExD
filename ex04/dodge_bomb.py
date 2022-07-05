fd7bbdb84f2bd808345acc9866bfc469a689c9b8
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-07-05 14:48:47 +0900

    5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,26 +1,35 @@
 import pygame as pg
 import sys
-
+import random
 def main():
+    # 練習１
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900)) # Suface
-    screen_rect = screen_sfc.get_rect() # Rect
+    screen_rct = screen_sfc.get_rect() # Rect
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg") # Suface
     bgimg_rct = bgimg_sfc.get_rect()   # Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    #clock.tick(0.5)
+    clock.tick(0.5)
     # 練習３
     kkimg_sfc = pg.image.load("fig/6.png") # Surface
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) # Surface
     kkimg_rct = kkimg_sfc.get_rect() # Rect
     kkimg_rct.center = 900, 400
+
+    # 練習５
+    bmimg_sfc = pg.Surface((20, 20))
+    bmimg_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10) # Surface
+    bmimg_rct = bmimg_sfc.get_rect()
+    bmimg_rct.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct.centery = random.randint(0, screen_rct.height)
+
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-        
-
-
+    
+   
         # 練習２
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -31,6 +40,8 @@ def main():
         if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= 1 # y座標を-1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1# y座標を-1
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+
         pg.display.update()
         clock.tick(1000)
 if __name__ == "__main__":
