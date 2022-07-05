fca7445574aac742bc1b1665bfb19b900ec2a3f2
miyaji <c0a21118e1@edu.teu.ac.jp>
2022-07-05 14:35:16 +0900

    4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -10,7 +10,7 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()   # Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    clock.tick(0.5)
+    #clock.tick(0.5)
     # 練習３
     kkimg_sfc = pg.image.load("fig/6.png") # Surface
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) # Surface
@@ -18,13 +18,19 @@ def main():
     kkimg_rct.center = 900, 400
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        
 
 
         # 練習２
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        # 練習4
+        key_states = pg.key.get_pressed() # 辞書
+        if key_states[pg.K_UP] == True: kkimg_rct.centery -= 1 # y座標を-1
+        if key_states[pg.K_DOWN] == True: kkimg_rct.centery += 1 # y座標を-1
+        if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= 1 # y座標を-1
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1# y座標を-1
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
         pg.display.update()
         clock.tick(1000)
 if __name__ == "__main__":
